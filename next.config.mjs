9d8a197405238d0ee0bf69a2298897f1114d28b6
Hibritu <163972005+Hibritu@users.noreply.github.com>
2026-07-17 13:45:12 +0300

    Update next.config.mjs for export settings

--- next.config.mjs
+++ next.config.mjs
@@ -1,4 +1,12 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
+const nextConfig = {
+  output: "export",
+  images: {
+    unoptimized: true,
+  },
+  basePath: "/portifolio",
+  assetPrefix: "/portifolio",
+  trailingSlash: true,
+};
 
 export default nextConfig;
